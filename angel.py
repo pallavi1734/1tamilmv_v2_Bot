b8fb2b494da1bcf36e5a0b8f5aeaf78f766c2cb4
pallavi1734 <pallavi30184@rediffmail.com>
2024-11-26 15:00:37 +0530

    Update angel.py

--- angel.py
+++ angel.py
@@ -6,7 +6,7 @@ import requests
 from bs4 import BeautifulSoup
 from flask import Flask, request
 
-TOKEN = '6700432608:AAGLewsKHozPU8WoAIzvdEtYLGUhqZAZw'  # replace your bot token
+TOKEN = '7331346379:AAHGai3XupFo9eFcsz0cbbEAflGTrgZcDEc'  # replace your bot token
 
 bot = telebot.TeleBot(TOKEN)
 
